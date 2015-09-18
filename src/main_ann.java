1dfc5b9686658abb4597ea2e508f8def5d9e5249
danielbrouwer08 <danielbrouwer08@hotmail.com>
2015-09-18 21:40:54 +0200

    alles verdeeld in een test, training en validation set en python script herschreven

--- src/main_ann.java
+++ src/main_ann.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class main_ann {
 //parameters of the network	
 private static int hiddenlayers_amount = 1;
-private static int neuronsperlayer = 60;
+private static int neuronsperlayer = 35;
 private static int incoming_amount = 10;
 private static int outgoing_amount = 7;
 private static double alpha = 0.1;
@@ -17,21 +17,15 @@ private static int epochs = 50;
 private static double[][] features = new double[7854][10];
 private static int[] targets = new int[7854];
 private static double[][][] fold = new double[7][(targets.length)/7][10];
-private static double[] testinputs = new double[10];
-private static double[] testoutputs = new double[7];
-private static double[] hiddenoutputs = new double[7];
-private static double[] desiredoutputs = new double[7];
-private static double[] errorvalues = new double[7];
-private static double[] outputgradients = new double[7];
-private static double sum_err_val = 0.0;
-private static double[] output_targets = new double[7854];
+private static int[][] targetfolds = new int[7][targets.length/7];
+private static double sum_square_error_test_fold;
+private static double sum_square_error_validation_fold;
 
 	public static void main(String[] args) {
     
  	    //Make a neural network named n1 with the selected parameters.
  		NeuronNetwork n1 = new NeuronNetwork(hiddenlayers_amount, neuronsperlayer, outgoing_amount, incoming_amount);
 		
- 		
 		//read features.txt and put into 2-dim array features
 		readFeatures();
 		
@@ -51,89 +45,23 @@ private static double[] output_targets = new double[7854];
 			}
 		}
 		
-		
-		
-		
-		
-		
-		
-		//run code for each epoch:
-		for(int i=0;i<=epochs-1;i++)
+		//divide targets in 7 'folds'
+		for(int f=0;f<7;f++)
 		{
-			System.out.println("epoch" + (i+1));
-			System.out.println();
-			//reset sum of errors every epoch
-			sum_err_val=0;
-		
-			//for every input line of features.txt
-			for(int k=0;k<7854;k++)
+			for(int k=0;k<num;k++)
 			{
-				//read 10 new values as inputs
-				for(int m=0;m<10;m++)
-				{
-					testinputs[m]=features[k][m];
-				}
-			
-				//put zero's in the desiredoutputs vector
-				for(int n=0;n<desiredoutputs.length;n++)
-				{	
-					desiredoutputs[n]=0.0;
-				}
-					
-				//only the desiredouput that corrosponds to the target is set to 1
-				desiredoutputs[targets[k]-1]=1.0;
-					
-				//calculate the outputs
-				//testoutputs = n1.calculate_Outputs(testinputs, n1.size()-1);
-				
-				//found outputs in targets.txt format for comparison later, highest value decides the number since use of sigmoid this outputs a integer instead of a double
-				//output_targets[k] = index_Highestvalue(testoutputs)+1;
-		
-				
-				//begin of backpropagation:
-				n1.update(desiredoutputs, alpha, testinputs);
-				
-				testoutputs = n1.calculate_Outputs(testinputs, hiddenlayers_amount+1);
-				
-				//calculate the errorvalues
-				errorvalues = calculate_Errorvalues(testoutputs,desiredoutputs);
-		
-				//calculate sum of all errors each epoch
-				for(int j=0;j<errorvalues.length;j++)
-				{
-					sum_err_val=sum_err_val+errorvalues[j]*errorvalues[j]; 
-				}
-		
-				
-				output_targets[k] = index_Highestvalue(testoutputs)+1;
-		
-			}//at this point 1 line of 10 input values is processed, returning to next line.
-		
-			//After each epoch:					
-			//print all outputs of epoch i
-			for(int j=0;j<testoutputs.length;j++)
-			{
-				System.out.println((j+1) + ". " + testoutputs[j]);
+				targetfolds[f][k]= targets[k];
 			}
-				
-			System.out.println();
-				
-			//print all errorvalues and compute the sum of the errorvalues
-			for(int j=0;j<errorvalues.length;j++)
-			{
-				System.out.println((j+1) + ". " + errorvalues[j]);
-			}
-				
-			//print sum of all errorvalues squared for each epoch
-			System.out.println();
-			System.out.println("Sum of errorvalues squared:" + (sum_err_val));
-			System.out.println();
+		}
+	
 		
-		}	
-		//store output_of targets of last epoch into a txt file
-		array_to_txtfile(output_targets);
+		//run testfold once, training fold for n epochs and validation fold for the network
+		run_test_fold(n1);
+		run_training_fold(n1, epochs);
+		run_validation_fold(n1);
 		
-		//store parameters into a txt file
+		
+		//put used parameters + sum of squared errors in a text file for distinguish data
 		data_to_txtfile();
 		
 		
@@ -141,12 +69,19 @@ private static double[] output_targets = new double[7854];
 	
 	public static void run_test_fold(NeuronNetwork n)
 	{
+		double[] testinputs = new double[10];
+		double[] testoutputs = new double[7];
+		double[] desiredoutputs = new double[7];
+		double[] errorvalues = new double[7];
+		double sum_err_val=0.0;
+		double[] output_targets = new double[(targets.length)/7];
+		
 		for(int k=0;k<(targets.length)/7;k++)
 		{
 			//read 10 new values as inputs
 			for(int m=0;m<10;m++)
 			{
-				testinputs[m]=features[k][m];
+				testinputs[m]=fold[0][k][m];
 			}
 		
 			//put zero's in the desiredoutputs vector
@@ -156,7 +91,7 @@ private static double[] output_targets = new double[7854];
 			}
 		
 			//only the desiredouput that corrosponds to the target is set to 1
-			desiredoutputs[targets[k]-1]=1.0;
+			desiredoutputs[targetfolds[0][k]-1]=1.0;
 				
 			//begin of backpropagation:
 			n.update(desiredoutputs, alpha, testinputs);
@@ -175,14 +110,122 @@ private static double[] output_targets = new double[7854];
 			output_targets[k] = index_Highestvalue(testoutputs)+1;
 	
 		}//at this point 1 line of 10 input values is processed, returning to next line.
+		
+		//print sum of all errorvalues squared for each epoch
+				System.out.println();
+				System.out.println("Sum of errorvalues squared for test fold:" + (sum_err_val));
+				System.out.println();
+				
+				sum_square_error_test_fold = sum_err_val;
+				
+				//put outputs into txt file
+				test_to_txtfile(output_targets);
+				
+				//put targets of test fold into txt file for comparison later	
+				testset_to_txtfile(targetfolds[0]);
 	}
 	
-	public static void array_to_txtfile(double[] data)
+	public static void run_training_fold(NeuronNetwork n, int epochs)
 	{
-		//Prints the data and epoch number to textfile output.txt
+		double[] testinputs = new double[10];
+		double[] desiredoutputs = new double[7];
+		
+		for(int g=0;g<epochs;g++)
+		{
+		//for all 5 training sets
+		for(int l=1;l<6;l++)
+		{
+		for(int k=0;k<(targets.length)/7;k++)
+		{
+			//read 10 new values as inputs
+			for(int m=0;m<10;m++)
+			{
+				testinputs[m]=fold[l][k][m];
+			}
+		
+			//put zero's in the desiredoutputs vector
+			for(int z=0;z<desiredoutputs.length;z++)
+			{	
+				desiredoutputs[z]=0.0;
+			}
+		
+			//only the desiredouput that corrosponds to the target is set to 1
+			desiredoutputs[targetfolds[l][k]-1]=1.0;
+				
+			//begin of backpropagation:
+			n.update(desiredoutputs, alpha, testinputs);
+	
+		}//at this point 1 line of 10 input values is processed, returning to next line.
+		}
+		}
+	}
+	
+	public static void run_validation_fold(NeuronNetwork n)
+	{
+		double[] testinputs = new double[10];
+		double[] testoutputs = new double[7];
+		double[] desiredoutputs = new double[7];
+		double[] errorvalues = new double[7];
+		double sum_err_val=0.0;
+		double[] output_targets = new double[(targets.length)/7];
+		
+		//for the last fold (validation fold)
+		for(int k=0;k<(targets.length)/7;k++)
+		{
+			//read 10 new values as inputs
+			for(int m=0;m<10;m++)
+			{
+				testinputs[m]=fold[6][k][m];
+			}
+		
+			//put zero's in the desiredoutputs vector
+			for(int z=0;z<desiredoutputs.length;z++)
+			{	
+				desiredoutputs[z]=0.0;
+			}
+		
+			//only the desiredouput that corrosponds to the target is set to 1
+			desiredoutputs[targetfolds[6][k]-1]=1.0;
+				
+			//begin of backpropagation:
+			n.update(desiredoutputs, alpha, testinputs);
+			
+			testoutputs = n.calculate_Outputs(testinputs, hiddenlayers_amount+1);
+			
+			//calculate the errorvalues
+			errorvalues = calculate_Errorvalues(testoutputs,desiredoutputs);
+	
+			//calculate sum of all errors
+			for(int j=0;j<errorvalues.length;j++)
+			{
+				sum_err_val=sum_err_val+errorvalues[j]*errorvalues[j]; 
+			}
+			
+			output_targets[k] = index_Highestvalue(testoutputs)+1;
+	
+		}//at this point 1 line of 10 input values is processed, returning to next line.
+		
+		//print sum of all errorvalues squared for each epoch
+		System.out.println();
+		System.out.println("Sum of errorvalues squared for validation fold:" + (sum_err_val));
+		System.out.println();
+		
+		sum_square_error_validation_fold=sum_err_val;
+		
+		//put outputs into txt file
+		validation_to_txtfile(output_targets);
+		
+		//put targets of validation fold into txt file for comparison later	
+		validationset_to_txtfile(targetfolds[6]);
+		
+		}
+	
+	public static void test_to_txtfile(double[] data)
+	{
+		//Prints the data to textfile test_output.txt
 		PrintWriter writer;
 		try {
-			writer = new PrintWriter("src\\output.txt", "UTF-8");
+			writer = new PrintWriter("src\\test_outputs.txt", "UTF-8");
 			for(int i=0; i<data.length;i++)
 			{
 				writer.println((int)data[i]);
@@ -196,7 +239,69 @@ private static double[] output_targets = new double[7854];
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+	}
+	
+	public static void validation_to_txtfile(double[] data)
+	{
+		//Prints the data to textfile validation_outputs.txt
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("src\\validation_outputs.txt", "UTF-8");
+			for(int i=0; i<data.length;i++)
+			{
+				writer.println((int)data[i]);
+			}
+			writer.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void validationset_to_txtfile(int[] data)
+	{
+		//Prints the data to textfile validation_targets.txt
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("src\\validation_targets.txt", "UTF-8");
+			for(int i=0; i<data.length;i++)
+			{
+				writer.println((int)data[i]);
+			}
+			writer.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void testset_to_txtfile(int[] data)
+	{
+		//Prints the data to textfile validation_targets.txt
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("src\\test_targets.txt", "UTF-8");
+			for(int i=0; i<data.length;i++)
+			{
+				writer.println((int)data[i]);
+			}
+			writer.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public static void data_to_txtfile()
@@ -223,6 +328,13 @@ private static double[] output_targets = new double[7854];
 				temp = "epochs: ";
 				temp = temp + (int)epochs;
 				writer.println((String)temp);
+				temp = "sum of squared errors in test fold:";
+				temp = temp + (double)sum_square_error_test_fold;
+				writer.println((String)temp);
+				temp = "sum of squared errors in validation fold:";
+				temp = temp + (double)sum_square_error_validation_fold;
+				writer.println((String)temp);
+				
 				
 			writer.close();
 			
